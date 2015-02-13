14c2fa8d6fec020b2d0d8d9033bd2577088303cc
DoJong <sonjw0109@naver.com>
2015-02-13 19:22:55 +0900

    edit user info

--- models/admin_model.php
+++ models/admin_model.php
@@ -5,6 +5,17 @@ Class Admin_model extends Model {
 			parent::__construct($db);
 		}
 	
+	function loadAdminInfo($info) {
+			$sql = 'SELECT * FROM admin_user where email = ?';
+			return $this->query_row($sql, $info);
+		
+	}
+	
+	function edit_user($info) {
+		$sql = 'UPDATE user SET email = ?, name = ?, group_id = ? where email = ?';
+		return $this->query_exec($sql, $info);
+	}
+	
 }
 
 ?>
---
+++ views/edit-user-admin.php
@@ -0,0 +1,31 @@
+					<div class="basebox pd_lr_10 pd_tb_5">
+						<div class="col-md-6 center_block pd_10">							
+							<form id="edit_form" action="<?=URL?>/gns_admin/edit_user_process" method="post" class="form-horizontal">
+								<div  class="form-group">
+									<label class="col-sm-4 control-label">E-Mail</label>
+									<div class="col-sm-8"><input type="text" name="user_email" class="form-control" value=<?php echo($user->email) ?>></div>
+								</div>
+								<div  class="form-group">
+									<label class="col-sm-4 control-label">Name</label>
+									<div class="col-sm-8"><input type="text" name="user_name" class="form-control" value=<?php echo($user->name) ?>></div>
+								</div>
+								<div  class="form-group">
+									<label class="col-sm-4 control-label">Group</label>
+									<div class="col-sm-8">
+										<select name="user_group_id" class="form-control">
+											<?php foreach($group as $row) { ?>
+											<option value="<?=$row->group_id?>" <?=($user->group_id==$row->group_id) ? "selected" : ""?>>
+												<?=$row->group_name?>
+											</option>
+											<?php } ?>											
+										</select>
+									</div>
+								</div>
+								<div class="form-group" style="margin-bottom: 0">				
+									<div class="col-md-12 text-right">
+										<button type="submit" class="btn btn-danger">Submit</button>
+									</div>
+								</div>
+							</form>						
+						</div>
+					</div>
