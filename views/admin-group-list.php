0209157ed7928d970951a355a826c7685ec36ddb
DoJong <sonjw0109@naver.com>
2015-02-15 20:26:51 +0900

    get user by group

---
+++ views/admin-group-list.php
@@ -0,0 +1,13 @@
+<div class="basebox pd_lr_10 pd_tb_5">
+	<div>
+		<select class="form-control">
+		  <option>1</option>
+		  <option>2</option>
+		  <option>3</option>
+		  <option>4</option>
+		  <option>5</option>
+		</select>
+	</div>
+</div>
+					
+					
