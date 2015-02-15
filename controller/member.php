2333311b5dbe32b43001b11901abb0169a60144c
jslee <zipoo91@naver.com>
2015-02-15 03:46:55 -0800

    thumbnail

--- controller/member.php
+++ controller/member.php
@@ -3,11 +3,11 @@
 Class Member extends Controller {
 	function __construct() {
 		parent::__construct();
+		$this->member_model = $this->loadModel('member_model');
 	}
 	
 	function signup() {
-		$model = $this->loadModel('member_model');
-		$group = $model->loadGroupInfo();
+		$group = $this->member_model->loadGroupInfo();
 		require './views/header-no-sidebar.php';
 		require './views/signup.php';
 		require './views/footer.php';	
@@ -20,8 +20,7 @@ Class Member extends Controller {
 	}
 	
 	function edit() {
-		$model = $this->loadModel('member_model');
-		$group = $model->loadGroupInfo();
+		$group = $this->member_model->loadGroupInfo();
 		require './views/header-no-sidebar.php';
 		require './views/edit.php';
 		require './views/footer.php';
@@ -29,16 +28,15 @@ Class Member extends Controller {
 	
 	function signup_process() {
 		$options = array('cost' => 11);
-		$model 			= $this->loadModel('member_model');
 		$email 			= $_POST['user_email'];
 		$name 			= $_POST['user_name'];
 		$password	 	= password_hash($_POST['user_password'], PASSWORD_BCRYPT, $options);
 		$group_id		= $_POST['user_group_id'];
-		$profile_image	= './images/no-profile.png';
+		$profile_image	= URL.'/images/no-profile.png';
 		
 		$info = array($email, $name, $password, $group_id, $profile_image);
 		
-		$res  = $model->regist($info);
+		$res  = $this->member_model->regist($info);
 		
 		if($res) {
 			echo '<script>
@@ -55,12 +53,11 @@ Class Member extends Controller {
 	}
 	
 	function login_process() {
-		$model		= $this->loadModel('member_model');
 		$email		= $_POST['user_email'];
 		$password	= $_POST['user_password'];
 		
 		$info = array($email);
-		$res = $model->loadUserInfo($info);
+		$res = $this->member_model->loadUserInfo($info);
 		
 		if($res == false) {
 			echo '<script>
@@ -88,10 +85,8 @@ Class Member extends Controller {
 	}
 	
 	function edit_process() {
-		$model = $this->loadModel('member_model');
-		
 		$info = array($_SESSION['user_email']);
-		$temp_res = $model->loadUserInfo($info);
+		$temp_res = $this->member_model->loadUserInfo($info);
 		$old_password = $_POST['user_old_password'];
 		
 		if(password_verify($old_password, $temp_res->password) == FALSE) {
@@ -101,18 +96,33 @@ Class Member extends Controller {
 			</script>';
 		}
 		else {
-			if($_FILES['user_profile_image']['size'] == 0)
-			{
-				$uploadfile = $_SESSION['user_profile_image'];
+			if($_POST['check_delete_image'] == "on") {
+				$uploadfile = URL.'/images/no-profile.png';
 			}
-			else{
-				$uploaddir = './upload/profile/'.$_SESSION['user_id'].'/';
-				if(file_exists($uploaddir) == false)
+			else {
+				if($_FILES['user_profile_image']['name'] == null)
 				{
-					mkdir("$uploaddir",0777); 
+					$uploadfile = $_SESSION['user_profile_image'];
 				}
-				$uploadfile = $uploaddir . basename($_FILES['user_profile_image']['name']);
-				move_uploaded_file($_FILES['user_profile_image']['tmp_name'], $uploadfile);
+				else{
+					$uploaddir = './upload/profile/'.$_SESSION['user_id'].'/';				
+					if(file_exists($uploaddir) == false)
+					{
+						mkdir("$uploaddir", 0777); 
+					}
+					$uploadfile = $uploaddir . basename($_FILES['user_profile_image']['name']);
+					if(move_uploaded_file($_FILES['user_profile_image']['tmp_name'], $uploadfile))
+					{
+						$thumb = $uploaddir.'/thumb_'. basename($_FILES['user_profile_image']['name']);
+						
+						if($this->make_thumbnail($uploadfile, 100, 100, $thumb)) {
+							$uploadfile = URL.'/upload/profile/'.$_SESSION['user_id'].'/'.'thumb_'. basename($_FILES['user_profile_image']['name']);
+						}
+						else {
+							$thumb = $uploaddir;
+						}
+					}
+				}
 			}
 			$options 		= array('cost' => 11);
 			$email      	= $_POST['user_email'];
@@ -122,14 +132,14 @@ Class Member extends Controller {
 				$password = $temp_res->password;
 			}
 			else {
-				$password 		= password_hash($_POST['user_new_password'], PASSWORD_BCRYPT, $options);
+				$password = password_hash($_POST['user_new_password'], PASSWORD_BCRYPT, $options);
 			}
 			
 			$group_id		= $_POST['user_group_id'];
 			$user_id 		= $_SESSION['user_id'];
 			$profile_image	= $uploadfile;
 			$info = array($email, $name, $password, $group_id, $profile_image, $user_id);
-			$res = $model->edit($info);
+			$res = $this->member_model->edit($info);
 			
 			if($res == TRUE) {
 				$_SESSION['user_id'] 			= $res->id;
@@ -147,6 +157,60 @@ Class Member extends Controller {
 				</script>';	
 			}
 		}
+	}
+	
+	function make_thumbnail($source_file, $_width, $_height, $object_file) {
+	    list($img_width,$img_height, $type) = getimagesize($source_file);
+	    if ($type==1) $img_sour = imagecreatefromgif($source_file);
+	    else if ($type==2 ) $img_sour = imagecreatefromjpeg($source_file);
+	    else if ($type==3 ) $img_sour = imagecreatefrompng($source_file);
+	    else if ($type==15) $img_sour = imagecreatefromwbmp($source_file);
+	    else return false;
+	    if ($img_width > $img_height) {
+	        $width = round($_height*$img_width/$img_height);
+	        $height = $_height;
+	    } else {
+	        $width = $_width;
+	        $height = round($_width*$img_height/$img_width);
+	    }
+	    if ($width < $_width) {
+	        $width = round(($height + $_width - $width)*$img_width/$img_height);
+	        $height = round(($width + $_width - $width)*$img_height/$img_width);
+	    } else if ($height < $_height) {
+	        $height = round(($width + $_height - $height)*$img_height/$img_width);
+	        $width = round(($height + $_height - $height)*$img_width/$img_height);
+	    }
+	    $x_last = round(($width-$_width)/2);
+	    $y_last = round(($height-$_height)/2);
+	    if ($img_width < $_width || $img_height < $_height) {
+	        $img_last = imagecreatetruecolor($_width, $_height); 
+	        $x_last = round(($_width - $img_width)/2);
+	        $y_last = round(($_height - $img_height)/2);
+	
+	        imagecopy($img_last,$img_sour,$x_last,$y_last,0,0,$width,$height);
+	        imagedestroy($img_sour);
+	        $white = imagecolorallocate($img_last,255,255,255);
+	        imagefill($img_last, 0, 0, $white);
+	    } else {
+	        $img_dest = imagecreatetruecolor($width,$height); 
+	        imagecopyresampled($img_dest, $img_sour,0,0,0,0,$width,$height,$img_width,$img_height); 
+	        $img_last = imagecreatetruecolor($_width,$_height); 
+	        imagecopy($img_last,$img_dest,0,0,$x_last,$y_last,$width,$height);
+	        imagedestroy($img_dest);
+	    }
+	    if ($object_file) {
+	        if ($type==1) imagegif($img_last, $object_file, 100);
+	        else if ($type==2 ) imagejpeg($img_last, $object_file, 100);
+	        else if ($type==3 ) imagepng($img_last, $object_file, 100);
+	        else if ($type==15) imagebmp($img_last, $object_file, 100);
+	    } else {
+	        if ($type==1) imagegif($img_last);
+	        else if ($type==2 ) imagejpeg($img_last);
+	        else if ($type==3 ) imagepng($img_last);
+	        else if ($type==15) imagebmp($img_last);
+	    }
+	    imagedestroy($img_last);
+	    return true;
 	}
 	
 	function logout() {
--- models/member_model.php
+++ models/member_model.php
@@ -1,42 +1,37 @@
 <?php
-	class Member_model extends Model {
-		function __construct($db) {
-			parent::__construct($db);
-		}
-		
-		function regist($info) {
-			$sql = 'INSERT INTO user(email, name, password, group_id, profile_image, regdate)
-					VALUES(?, ?, ?, ?, ?, now())';
-			return $this->query_exec($sql, $info);
-		}
-		
-		function getUsers() {
-			$sql = 'SELECT * FROM user';
-			return $this->query_result($sql, null);
-		}
-		
-		function getUserById($id) {
-			$sql = 'SELECT * FROM user where id = ?';
-			return $this->query_row($sql, array($id));
-			
-		}
-		
-		function loadGroupInfo() {
-			$sql = 'SELECT * FROM groups';
-			return $this->query_result($sql, null);
-		}
-		
-		function loadUserInfo($info) {
-			$sql = 'SELECT * FROM user where email = ?';
-			return $this->query_row($sql, $info);
-			
-		}
-		
-		function edit($info) {
-			$sql1 = 'UPDATE user SET email = ?, name = ?, password =?, group_id = ?, profile_image = ?, regdate = now() where id = ?';					
-			$this->query_exec($sql1, $info);
-			$sql2 = 'SELECT * FROM user where email = ?';
-			return $this->query_row($sql2, array($info[0]));
-		}
+
+class Member_model extends Model {
+	function __construct($db) {
+		parent::__construct($db);
 	}
+	
+	function regist($info) {
+		$sql = 'INSERT INTO user(email, name, password, group_id, profile_image, regdate)
+				VALUES(?, ?, ?, ?, ?, now())';
+		return $this->query_exec($sql, $info);
+	}
+	
+	function loadGroupInfo() {
+		$sql = 'SELECT * FROM groups';
+		return $this->query_result($sql, null);
+	}
+	
+	function loadUserInfo($info) {
+		$sql = 'SELECT * FROM user where email = ?';
+		return $this->query_row($sql, $info);
+		
+	}
+	
+	function loadEmailInfo($info) {
+		$sql = 'SELECT email FROM user';
+		return $this->query_result($sql, null); 
+	}
+	
+	function edit($info) {
+		$sql1 = 'UPDATE user SET email = ?, name = ?, password =?, group_id = ?, profile_image = ?, regdate = now() where id = ?';					
+		$this->query_exec($sql1, $info);
+		$sql2 = 'SELECT * FROM user where email = ?';
+		return $this->query_row($sql2, array($info[0]));
+	}
+}
 ?>
