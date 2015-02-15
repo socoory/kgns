ec6fc2f2dd59264efe067bd1aee34e9bbfa6c6b9
benimario <benimarioes@gmail.com>
2015-02-15 21:09:59 +0900

    bug fix

--- models/member_model.php
+++ models/member_model.php
@@ -1,44 +1,4 @@
 <?php
-<<<<<<< HEAD
-	class Member_model extends Model {
-		function __construct($db) {
-			parent::__construct($db);
-		}
-		
-		function regist($info) {
-			$sql = 'INSERT INTO user(email, name, password, group_id, profile_image, regdate)
-					VALUES(?, ?, ?, ?, ?, now())';
-			return $this->query_exec($sql, $info);
-		}
-		
-		function getUsers() {
-			$sql = 'SELECT * FROM user';
-			return $this->query_result($sql, null);
-		}
-		
-		function getUserById($id) {
-			$sql = 'SELECT * FROM user where id = ?';
-			return $this->query_row($sql, array($id));			
-		}
-		
-		function getUserByGroup($group) {
-			$sql = 'SELECT * FROM user where group_id = ?';
-			return $this->query_result($sql, $group);
-			
-		}
-		
-		function loadGroupInfo() {
-			$sql = 'SELECT * FROM groups';
-			return $this->query_result($sql, null);
-		}
-		
-		function loadUserInfo($info) {
-			$sql = 'SELECT * FROM user where email = ?';
-			return $this->query_row($sql, $info);
-			
-		}
-=======
-
 class Member_model extends Model {
 	function __construct($db) {
 		parent::__construct($db);
@@ -50,6 +10,22 @@ class Member_model extends Model {
 		return $this->query_exec($sql, $info);
 	}
 	
+	function getUsers() {
+		$sql = 'SELECT * FROM user';
+		return $this->query_result($sql, null);
+	}
+	
+	function getUserById($id) {
+		$sql = 'SELECT * FROM user where id = ?';
+		return $this->query_row($sql, array($id));			
+	}
+	
+	function getUserByGroup($group) {
+		$sql = 'SELECT * FROM user where group_id = ?';
+		return $this->query_result($sql, $group);
+		
+	}
+	
 	function loadGroupInfo() {
 		$sql = 'SELECT * FROM groups';
 		return $this->query_result($sql, null);
@@ -58,7 +34,6 @@ class Member_model extends Model {
 	function loadUserInfo($info) {
 		$sql = 'SELECT * FROM user where email = ?';
 		return $this->query_row($sql, $info);
->>>>>>> member_module
 		
 	}
 	
