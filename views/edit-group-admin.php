a67f284d9ad1e56b87bb8c42bd358d32d36e55f0
DoJong <sonjw0109@naver.com>
2015-02-25 19:43:17 +0900

    e

---
+++ views/edit-group-admin.php
@@ -0,0 +1,22 @@
+<div class="basebox pd_lr_10 pd_tb_5">
+	<div class="col-md-6 center_block pd_10">							
+		<form id="edit_form" action="<?=URL?>/gns_admin/edit_group_process" method="post" class="form-horizontal">
+			<input type="hidden" name="group_id" value=<?php echo($group->group_id) ?> />
+			<div  class="form-group">
+				<fieldset disabled>
+					<label class="col-sm-4 control-label">Group ID</label>
+					<div class="col-sm-8"><input type="text" name="group_id" class="form-control" value=<?php echo($group->group_id) ?>></div>
+				</fieldset>
+			</div>
+			<div  class="form-group">
+				<label class="col-sm-4 control-label">Group Name</label>
+				<div class="col-sm-8"><input type="text" name="group_name" class="form-control" value=<?php echo($group->group_name) ?>></div>
+			</div>			
+			<div class="form-group" style="margin-bottom: 0">				
+				<div class="col-md-12 text-right">
+					<button type="submit" class="btn btn-danger">Submit</button>
+				</div>
+			</div>
+		</form>						
+	</div>
+</div>
